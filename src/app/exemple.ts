18ab28160137509d268cb2c253d3893183f25b49
dymafr <32801166+dymafr@users.noreply.github.com>
2022-12-05 14:53:46 +0100

    1 file modified

--- src/app/exemple.ts
+++ src/app/exemple.ts
@@ -1,26 +1,26 @@
-import { HttpClient } from "@angular/common/http";
-import { Component, ViewChild } from "@angular/core";
-import { MatPaginator } from "@angular/material/paginator";
-import { MatSort } from "@angular/material/sort";
-import { MatTableDataSource } from "@angular/material/table";
-import { Observable, of } from "rxjs";
-import { catchError, map } from "rxjs/operators";
+import { HttpClient } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Component({
-  selector: "app-exemple-component",
-  templateUrl: "exemple.html",
-  styleUrls: ["exemple.scss"]
+  selector: 'app-exemple-component',
+  templateUrl: 'exemple.html',
+  styleUrls: ['exemple.scss'],
 })
 export class ExempleComponent {
-  displayedColumns = ["created", "state", "number", "title"];
-  exampleDatabase: ExampleHttpDao | null;
+  displayedColumns = ['created', 'state', 'number', 'title'];
+  exampleDatabase!: ExampleHttpDao;
   dataSource = new MatTableDataSource();
 
   resultsLength = 0;
   isLoading = true;
 
-  @ViewChild(MatPaginator, { static: true }) paginateur: MatPaginator;
-  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  @ViewChild(MatPaginator, { static: true }) paginateur!: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort!: MatSort;
 
   constructor(private http: HttpClient) {}
 
@@ -31,9 +31,9 @@ export class ExempleComponent {
     this.sort.sortChange.subscribe(() => (this.paginateur.pageIndex = 0));
 
     this.exampleDatabase = new ExampleHttpDao(this.http);
-    this.exampleDatabase!.getRepoIssues("created", "desc", 1)
+    this.exampleDatabase!.getRepoIssues('created', 'desc', 1)
       .pipe(
-        map(data => {
+        map((data) => {
           this.isLoading = false;
           this.resultsLength = data.total_count;
           return data.items.slice(0, 40);
@@ -43,7 +43,7 @@ export class ExempleComponent {
           return of([]);
         })
       )
-      .subscribe(data => {
+      .subscribe((data) => {
         this.dataSource.data = data;
       });
   }
@@ -69,9 +69,10 @@ export class ExampleHttpDao {
     order: string,
     page: number
   ): Observable<GithubApi> {
-    const href = "https://api.github.com/search/issues";
-    const requestUrl = `${href}?q=repo:angular/components&sort=${sort}&order=${order}&page=${page +
-      1}`;
+    const href = 'https://api.github.com/search/issues';
+    const requestUrl = `${href}?q=repo:angular/components&sort=${sort}&order=${order}&page=${
+      page + 1
+    }`;
 
     return this._httpClient.get<GithubApi>(requestUrl);
   }
